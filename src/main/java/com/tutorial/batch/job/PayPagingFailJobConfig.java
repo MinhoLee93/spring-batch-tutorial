7f0311c5b3f5f30050baba8067dc3a1d3105f70a
Minho Lee <alsgkgk77@gmail.com>
2020-02-14 16:34:31 +0900

    스프링 배치 튜토리얼 완료

---
+++ src/main/java/com/tutorial/batch/job/PayPagingFailJobConfig.java
@@ -0,0 +1,88 @@
+package com.tutorial.batch.job;
+
+import com.tutorial.batch.domain.PaySuccess;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.database.JdbcCursorItemReader;
+import org.springframework.batch.item.database.JpaItemWriter;
+import org.springframework.batch.item.database.JpaPagingItemReader;
+import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
+import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import sun.dc.pr.PRError;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+@Slf4j
+@RequiredArgsConstructor
+@Configuration
+@ConditionalOnProperty(name = "job.name", havingValue = "payCursorJob")
+public class PayPagingFailJobConfig {
+    public static final String JOB_NAME = "payCursorJob";
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    private final EntityManagerFactory entityManagerFactory;
+    private final DataSource dataSource;
+
+    private final int chunkSize = 10;
+
+    @Bean
+    public Job payPagingJob() {
+        log.info(">>>>>>>>>>>>>>>>>>>>>>>>>>");
+        return jobBuilderFactory.get(JOB_NAME)
+                .start(payPagingStep())
+                .build();
+    }
+
+    @Bean
+    @JobScope
+    public Step payPagingStep() {
+        return stepBuilderFactory.get("payPagingStep")
+                .<PaySuccess, PaySuccess>chunk(chunkSize)
+                .reader(payPagingReader())
+                .processor(payPagingProcessor())
+                .writer(writer())
+                .build();
+    }
+
+    @Bean
+    @StepScope
+    public JdbcCursorItemReader<PaySuccess> payPagingReader() {
+        return new JdbcCursorItemReaderBuilder<PaySuccess>()
+                .sql("SELECT * FROM pay_success p WHERE p.success = false")
+                .rowMapper(new BeanPropertyRowMapper<>(PaySuccess.class))
+                .fetchSize(chunkSize)
+                .dataSource(dataSource)
+                .name("payPagingReader")
+                .build();
+    }
+
+    @Bean
+    @StepScope
+    public ItemProcessor<PaySuccess, PaySuccess> payPagingProcessor() {
+        return item -> {
+            item.success();
+            return item;
+        };
+    }
+
+    @Bean
+    @StepScope
+    public JpaItemWriter<PaySuccess> writer() {
+        JpaItemWriter<PaySuccess> writer = new JpaItemWriter<>();
+        writer.setEntityManagerFactory(entityManagerFactory);
+        return writer;
+    }
+}
--- src/main/java/com/tutorial/batch/job/SimpleJobConfig.java
+++ src/main/java/com/tutorial/batch/job/SimpleJobConfig.java
@@ -8,31 +8,53 @@ import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.repository.NoRepositoryBean;
+
+import static com.tutorial.batch.job.PayPagingFailJobConfig.JOB_NAME;
 
 @Slf4j
 @RequiredArgsConstructor
 @Configuration
+@ConditionalOnProperty(name = "job.name", havingValue = "simpleJob")
 public class SimpleJobConfig {
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
-    private final Logger logger = LoggerFactory.getLogger(SimpleJobConfig.class);
-
     @Bean
+    @JobScope
     public Job simpleJob(){
+        log.info(">>>>>>>>>>>>>> definition simpleJob");
         return jobBuilderFactory.get("simpleJob")
                 .start(simpleStep1())
                 .build();
     }
 
+    private final SimpleJobTasklet tasklet1;
+
+    @Bean
+    @StepScope
     public Step simpleStep1(){
+        log.info(">>>>>>>>>>>>>> definition simpleStep1");
+        return stepBuilderFactory.get("simpleStep1")
+                .tasklet(tasklet1)
+                .build();
+    }
+
+    @Bean
+    @StepScope
+    public Step simpleStep2(@Value("#{jobParameters[requestDate]}") String requestDate){
         return stepBuilderFactory.get("simpleStep1")
                 .tasklet((contribution , chunkContext )->{
-                    logger.info(">>>>> This is Step1");
+                    log.info(">>>>>>> This is Step2");
+                    log.info(">>>>>>> requestDate= {}", requestDate);
                     return RepeatStatus.FINISHED;
                 })
                 .build();
